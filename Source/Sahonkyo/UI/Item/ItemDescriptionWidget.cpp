c3fe5e22d704c2cad9fed2c0ac5574f223af82ff
meo-young <meoyoung000@naver.com>
2025-11-20 13:32:53 +0900

    [Update] Description UI 구현

---
+++ Source/Sahonkyo/UI/Item/ItemDescriptionWidget.cpp
@@ -0,0 +1,2 @@
+#include "UI/Item/ItemDescriptionWidget.h"
+
---
+++ Source/Sahonkyo/UI/Item/ItemDescriptionWidget.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetBase.h"
+#include "ItemDescriptionWidget.generated.h"
+
+UCLASS()
+class SAHONKYO_API UItemDescriptionWidget : public UWidgetBase
+{
+	GENERATED_BODY()
+	
+};
